6b7b35af6b008ec635f8091fe2e314b299421c16
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2021-12-24 17:43:49 +0200

    adding api calls is so easy now

--- imgur_client.py
+++ imgur_client.py
@@ -46,7 +46,14 @@ class ImgurClient:
     AUTH_CACHE = "auth_cache.ini"
 
     def __init__(self):
-        """Tries to read refresh token from auth cache"""
+        """
+        Reads refresh token from auth cache and gets a new token.
+        If no credentials are found, then creates an empty config file.
+        If only the refresh token is missing it triggers the whole application
+        authorization flow.
+        At the end of each successful authorization or token refresh the new
+        credentials are saved to the auth_cache file.
+        """
 
         if os.path.isfile(self.AUTH_CACHE):
             auth_cache = configparser.ConfigParser()
@@ -76,12 +83,14 @@ class ImgurClient:
         print(f"- authenticated as {self.username}")
 
     def cache_credentials(self, credentials):
+        """Saves provided credentials to auth cache file"""
         with open(self.AUTH_CACHE, "w") as storage:
             auth_cache = configparser.ConfigParser()
             auth_cache["credentials"] = credentials
             auth_cache.write(storage)
 
     def authorize(self, client_id, client_secret):
+        """Starts the authorization flow with imgur"""
         print("- launching browser to authorize application access")
         url = (
             f"{self.API_ROOT}/oauth2/authorize?client_id={client_id}&response_type=pin"
@@ -105,6 +114,7 @@ class ImgurClient:
         return response.json()
 
     def get_access_token(self, credentials):
+        """Having a refresh token, gets an access_token """
         route = "oauth2/token"
         payload = dict(
             **credentials,
@@ -118,6 +128,7 @@ class ImgurClient:
         return response.json()
 
     def authenticate(self, credentials):
+        """Attempts authentication with given credentials"""
         if credentials["refresh_token"] is None:
             del credentials["refresh_token"]
             accees_data = self.authorize(**credentials)
@@ -126,6 +137,12 @@ class ImgurClient:
         return accees_data
 
     def api_get(self, api, params=None, data=None):
+        """
+        Runs a GET request to imgur api.
+        On success it returns an ImmutableData object, to allow accessing
+        dictionary response keys as attributes.
+        On failure it raises apropriate HttpErrors.
+        """
         url = f"{self.API_V3}/{api}"
 
         response = requests.get(url, params, data=data, headers=self.auth_headers)
@@ -142,17 +159,30 @@ class ImgurClient:
         return response_data
 
     def me(self):
+        """Get current user details"""
         api_url = f"account/{self.username}"
         return self.api_get(api_url)
 
     def list_submissions(self, page=0, sort="newest"):
         """
-        Return the images a user has submitted to the gallery.
-        sort: newest (default), oldest, worst, best.
+        Return all image submissions to the gallery.
+
+        Keyword arguments:
+        page -- which page to retrieve, for larger results (default: 0)
+        sort -- newest (default), oldest, worst, best.
         """
         api_url = f"account/{self.username}/submissions/{page}/{sort}"
         return self.api_get(api_url)
 
+    def list_images(self):
+        api_url = "account/me/images"
+        return self.api_get(api_url)
+
+    def get_image(self, image_id, username=None):
+        if username is None:
+            username = "me"
+        api_url = f"account/{username}/image/{image_id}"
+        return self.api_get(api_url)
 
 if __name__ == "__main__":
     client = ImgurClient()
@@ -164,3 +194,7 @@ if __name__ == "__main__":
     submissions = client.list_submissions()
     for i, img in enumerate(submissions):
         print(i, img.title, img.views, img.points, img.link)
+
+    images = client.list_images()
+    for i, img in enumerate(images):
+        print(i, img.id, img.title, img.views, img.link)
